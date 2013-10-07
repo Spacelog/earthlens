acb7591c68d3849e85030766963a4c0b99b0b3cd
Mazz Mosley <marianne.mosley@gmail.com>
2013-10-07 12:57:32 +0100

    Alert presence of funky values and don't die

--- core/management/commands/ingest.py
+++ core/management/commands/ingest.py
@@ -1,4 +1,5 @@
 import datetime
+import traceback
 from bs4 import BeautifulSoup
 from django.core.management import BaseCommand
 from core.models import Image, Mission
@@ -11,7 +12,11 @@ class Command(BaseCommand):
 
     def handle(self, *files, **kwargs):
         for file in files:
-            self.ingest_file(file)
+            try:
+                self.ingest_file(file)
+            except:
+                traceback.print_exc()
+                raise
 
     def ingest_file(self, file):
         # Soup that file
@@ -33,8 +38,12 @@ class Command(BaseCommand):
             elif line.lower().startswith("features"):
                 details['features_text'] = line.split(":", 1)[1].strip()
             elif line.lower().startswith("center point latitude"):
-                details['latitude'] = float(line.split(":", 1)[1].split()[0].strip().strip(","))
-                details['longitude'] = float(line.split(":", 2)[2].split()[0].strip().strip(","))
+                try:
+                    details['latitude'] = float(line.split(":", 1)[1].split()[0].strip().strip(","))
+                    details['longitude'] = float(line.split(":", 2)[2].split()[0].strip().strip(","))
+                except ValueError:
+                    print "something funky in lat long that is not a number"
+                    pass
             elif last.lower().startswith("camera tilt"):
                 details['tilt_text'] = line
             elif line.lower().startswith("camera focal length"):
@@ -48,29 +57,49 @@ class Command(BaseCommand):
             elif line.lower().startswith("percentage of cloud cover"):
                 details['cloud_cover_text'] = int(line.split(":", 1)[1].split()[0].strip())
             elif line.lower().startswith("nadir point latitude"):
-                details['nadir_latitude'] = float(line.split(":", 1)[1].split()[0].strip().strip(","))
-                details['nadir_longitude'] = float(line.split(":", 2)[2].split()[0].strip().strip(","))
+                try:
+                    details['nadir_latitude'] = float(line.split(":", 1)[1].split()[0].strip().strip(","))
+                    details['nadir_longitude'] = float(line.split(":", 2)[2].split()[0].strip().strip(","))
+                except ValueError:
+                    print 'nadir data is funked'
+                    pass
             elif last.lower().startswith("nadir to photo"):
                 details['nadir_to_photo_text'] = line
             elif line.lower().startswith("sun azimuth"):
-                details['sun_azimuth'] = int(line.split(":", 1)[1].split()[0].strip())
+                try:
+                    details['sun_azimuth'] = int(line.split(":", 1)[1].split()[0].strip())
+                except ValueError:
+                    print 'sun azimuth is whack'
+                    pass
             elif line.lower().startswith("sun elevation angle"):
-                details['sun_elevation'] = int(line.split(":", 1)[1].split()[0].strip())
+                try:
+                    details['sun_elevation'] = int(line.split(":", 1)[1].split()[0].strip())
+                except ValueError:
+                    print 'the sun has elevated out of a sensible value'
+                    pass
             elif line.lower().startswith("spacecraft altitude"):
-                details['altitude'] = int(line.split(":", 1)[1].split()[0].strip()) * 1852
+                try:
+                    details['altitude'] = int(line.split(":", 1)[1].split()[0].strip()) * 1852
+                except ValueError:
+                    print 'altitude alti-wat'
+                    pass
             elif line.lower().startswith("gmt date"):
                 details['date_text'] = line
                 date_string = line.split(":")[1].split()[0].strip()
                 time_string = line.split(":")[2].split()[0].strip()
                 if date_string.endswith("____"):
-                    details['date'] = datetime.datetime(date_string[:4], 1, 1)
-                    details['date_start'] = datetime.datetime(date_string[:4], 1, 1)
-                    details['date_end'] = datetime.datetime(date_string[:4], 12, 31)
+                    try:
+                        details['date'] = datetime.datetime(date_string[:4], 1, 1)
+                        details['date_start'] = datetime.datetime(date_string[:4], 1, 1)
+                        details['date_end'] = datetime.datetime(date_string[:4], 12, 31)
+                    except TypeError:
+                        print 'date is funked up'
+                        pass
                 else:
                     details['date'] = datetime.datetime.strptime(date_string + "-" + time_string, "%Y%m%d-%H%M%S")
             last = line
         # Make the models
         mission = Mission.objects.get_or_create(code=details['mission'])[0]
         details['mission'] = mission
-        image = Image.objects.create(**details)
+        image = Image.objects.get_or_create(**details)
         print image
