6d9ce1a881eeca3cd5efc0f73acd9329c93b4784
Mazz Mosley <marianne.mosley@gmail.com>
2013-10-07 13:21:07 +0100

    Display the DATAS

--- core/views.py
+++ core/views.py
@@ -1,10 +1,10 @@
-from django.views.generic import TemplateView
+from django.views.generic.base import TemplateView
 from core.models import Image
 
 class IndexView(TemplateView):
     template_name = "index.html"
 
-    def get_context(self):
-        return {
-            "images": Image.objects.order_by("-rating")[:12],
-        }
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context["images"] = Image.objects.order_by("-rating")[:12]
+        return context
