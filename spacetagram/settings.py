7606f2cc678e7c44d4a0de094a20b65394897372
Andrew Godwin <andrew@aeracode.org>
2013-10-08 16:52:39 +0100

    Tweet button

--- spacetagram/settings.py
+++ spacetagram/settings.py
@@ -138,6 +138,18 @@ LOGGING = {
     }
 }
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.core.context_processors.tz',
+    'django.core.context_processors.request',
+    'django.contrib.messages.context_processors.messages',
+)
+
+
 LOGIN_REDIRECT_URL = "/rate/"
 LOGIN_URL = "/login/"
 
