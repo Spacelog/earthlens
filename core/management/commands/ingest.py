72a6816bea6da503d667ebd10fa8888f23f456b8
Andrew Godwin <andrew@aeracode.org>
2013-10-09 11:02:15 +0100

    Caption extraction

--- core/management/commands/ingest.py
+++ core/management/commands/ingest.py
@@ -2,6 +2,7 @@ import datetime
 import traceback
 from bs4 import BeautifulSoup
 from django.core.management import BaseCommand
+from django.utils.termcolors import colorize
 from core.models import Image, Mission
 
 
@@ -14,7 +15,7 @@ class Command(BaseCommand):
         for file in files:
             try:
                 self.ingest_file(file)
-            except:
+            except StandardError:
                 traceback.print_exc()
                 raise
 
@@ -27,10 +28,19 @@ class Command(BaseCommand):
         lines = text.split("\n")
         # Walk through lines getting data
         last = ""
-        details = {}
+        details = {'caption_text': None}
+        missing = set()
+        in_caption = False
         for line in lines:
             line = line.strip()
-            if last.lower().endswith("display record"):
+            if in_caption:
+                if line.lower().startswith("download packaged file") or line.lower().startswith("no captions available"):
+                    in_caption = False
+                    details['caption_text'] = ((details['caption_text'] + "\n") if details['caption_text'] else None)
+                elif line:
+                    print line
+                    details['caption_text'] += line + "\n"
+            elif last.lower().endswith("display record"):
                 details['code'] = line
                 details['mission'] = line.split("-")[0]
             elif line.lower().startswith("country or geog"):
@@ -42,8 +52,7 @@ class Command(BaseCommand):
                     details['latitude'] = float(line.split(":", 1)[1].split()[0].strip().strip(","))
                     details['longitude'] = float(line.split(":", 2)[2].split()[0].strip().strip(","))
                 except ValueError:
-                    print "something funky in lat long that is not a number"
-                    pass
+                    missing.add("center")
             elif last.lower().startswith("camera tilt"):
                 details['tilt_text'] = line
             elif line.lower().startswith("camera focal length"):
@@ -58,35 +67,30 @@ class Command(BaseCommand):
                 try:
                     details['cloud_cover_text'] = int(line.split(":", 1)[1].split()[0].strip())
                 except ValueError:
-                    print "too much computer in cloude"
-                    pass
+                    missing.add("cloud_cover")
             elif line.lower().startswith("nadir point latitude"):
                 try:
                     details['nadir_latitude'] = float(line.split(":", 1)[1].split()[0].strip().strip(","))
                     details['nadir_longitude'] = float(line.split(":", 2)[2].split()[0].strip().strip(","))
                 except ValueError:
-                    print 'nadir data is funked'
-                    pass
+                    missing.add("nadir")
             elif last.lower().startswith("nadir to photo"):
                 details['nadir_to_photo_text'] = line
             elif line.lower().startswith("sun azimuth"):
                 try:
                     details['sun_azimuth'] = int(line.split(":", 1)[1].split()[0].strip())
                 except ValueError:
-                    print 'sun azimuth is whack'
-                    pass
+                    missing.add("sun_azimuth")
             elif line.lower().startswith("sun elevation angle"):
                 try:
                     details['sun_elevation'] = int(line.split(":", 1)[1].split()[0].strip())
                 except ValueError:
-                    print 'the sun has elevated out of a sensible value'
-                    pass
+                    missing.add("sun_elevation")
             elif line.lower().startswith("spacecraft altitude"):
                 try:
                     details['altitude'] = int(line.split(":", 1)[1].split()[0].strip()) * 1852
                 except ValueError:
-                    print 'altitude alti-wat'
-                    pass
+                    missing.add("altitude")
             elif line.lower().startswith("gmt date"):
                 details['date_text'] = line
                 date_string = line.split(":")[1].split()[0].strip()
@@ -97,24 +101,33 @@ class Command(BaseCommand):
                         details['date_start'] = datetime.datetime(date_string[:4], 1, 1)
                         details['date_end'] = datetime.datetime(date_string[:4], 12, 31)
                     except TypeError:
-                        print 'date is funked up'
-                        pass
+                        missing.add("date")
                 elif time_string.startswith("(HH"):
                     try:
                         details['date'] = datetime.datetime.strptime(date_string, "%Y%m%d")
                         details['date_start'] = details['date']
                         details['date_end'] = details['date'] + details['date'].replace(hour=23, minute=59, second=59)
                     except TypeError:
-                        print 'date is funked up'
-                        pass
+                        missing.add("date")
                 else:
                     details['date'] = datetime.datetime.strptime(date_string + "-" + time_string, "%Y%m%d-%H%M%S")
+            elif line.lower().startswith("captions"):
+                in_caption = True
+                details['caption_text'] = ""
             last = line
         # Make the models
         if "mission" not in details:
-            print "mission say wat run into a problem"
+            print colorize("No mission in file %s" % file, fg="red")
             return
         mission = Mission.objects.get_or_create(code=details['mission'])[0]
-        details['mission'] = mission
-        image = Image.objects.get_or_create(**details)
-        print image
+        del details['mission']
+        image, new = Image.objects.get_or_create(code=details['code'], mission=mission)
+        for key, value in details.items():
+            setattr(image, key, value)
+        image.save()
+        if new:
+            print colorize("Ingested new image %s" % image, fg="green")
+        else:
+            print colorize("Updated existing image %s" % image, fg="cyan")
+        if missing:
+            print colorize("Image was missing: %s" % (", ".join(missing)))
